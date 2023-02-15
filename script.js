867514cb57aa6a740c43edea4ca3406f3f8fa327
Sohaib Shabbir <sohaib.shabbir@markinson.com.au>
2023-02-15 14:27:07 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// alert("linked file");
+
+let intiger = 3;
+let floating = 6.67
+
+// console.log(intiger+floating);
+
+// console.log('1'+1);
+// console.log('1'-1);
+
+if (intiger > 3) {
+    console.log("floating * 5", floating * 5)
+} else {
+    console.log("floating * 2", floating * 2);
+}
+function runMe(a) {
+    let x = "text"
+    let y = "text2"
+    console.log(x + " " + y)
+
+    console.log("10".concat(20))
+}
+runMe(2);
+
+console.log('a'>'b')
